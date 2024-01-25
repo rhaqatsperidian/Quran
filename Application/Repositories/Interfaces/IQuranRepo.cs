c9d558cce8cf35a88d693c1c1e4c57c6ab16f45d
Rizwan Saeedi <143165831+rhaqatsperidian@users.noreply.github.com>
2024-01-25 08:40:05 +0500

    update

--- Application/Repositories/Interfaces/IQuranRepo.cs
+++ Application/Repositories/Interfaces/IQuranRepo.cs
@@ -5,7 +5,7 @@ namespace Application.Repositories.Interfaces
 {
     public interface IQuranRepo
     {
-        Task<IEnumerable<QuranData>> GetQuranDataBySurah(int SurahNumber);
+        Task<IEnumerable<QuranData>> GetQuranDataBySurah(int SurahId);
         Task<IEnumerable<SurahListDto>> GetSurahList(bool arabicNames = true);
     }
 }
--- Application/Repositories/QuranRepo.cs
+++ Application/Repositories/QuranRepo.cs
@@ -24,10 +24,10 @@ namespace Application.Repositories
             }
         }
 
-        public Task<IEnumerable<QuranData>> GetQuranDataBySurah(int SurahNumber)
+        public Task<IEnumerable<QuranData>> GetQuranDataBySurah(int SurahId)
         {
             var sql = $@"SELECT * FROM {TableNameProvider<QuranData>.GetTableName()}
-                                                        WHERE {nameof(QuranData.surah_id)} = {SurahNumber}
+                                                        WHERE {nameof(QuranData.surah_id)} = {SurahId}
                                                         ORDER BY {nameof(QuranData.aya_id)} ASC";
             return GetDb().QueryAsync<QuranData>(sql);
         }
